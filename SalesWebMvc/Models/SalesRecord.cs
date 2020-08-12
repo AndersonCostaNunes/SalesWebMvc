c2366c990bb28ce37461df9c5ecae6a697d8f7a7
AndersonCostaNunes <anderson.costa.nunes@outlook.com>
2020-08-12 00:39:13 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -10,6 +10,7 @@ namespace SalesWebMvc.Models
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
+        public int SellerId { get; set; }
 
         public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
