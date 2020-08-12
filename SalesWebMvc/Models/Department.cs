1591d9478840dc9ba971b0482c150f8f045577c3
AndersonCostaNunes <anderson.costa.nunes@outlook.com>
2020-08-11 22:46:58 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,16 +9,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
-
-        public Department()
-        {
-        }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
         public Department(int id, string name)
         {
             Id = id;
             Name = name;
+        }
+        public Department()
+        {
+
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
 }
